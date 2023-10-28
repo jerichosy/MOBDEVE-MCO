075aa26924e49d598a7c9495404229d23649aa5c
Ced <79917173+Revkxd@users.noreply.github.com>
2023-10-28 19:34:41 +0800

    settings page complete

---
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/settings/SettingsAdapter.java
@@ -0,0 +1,54 @@
+package com.mobdeve.s17.samirsattendanceapp.ui.settings;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobdeve.s17.samirsattendanceapp.ClassData;
+import com.mobdeve.s17.samirsattendanceapp.R;
+import com.mobdeve.s17.samirsattendanceapp.ui.settings;
+
+public class SettingsAdapter {
+
+    ClassData[] settingsData;
+
+    public SettingsAdapter(ClassData[] settingsData) {
+        this.settingsData = settingsData;
+    }
+
+    @NonNull
+    @Override
+    public SettingsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        // TODO: Not sure if importing R like this instead of using context.getResources().getStringArray(R...) is okay
+        View view = inflater.inflate(R.layout.leaderboard_item_list, parent, false);
+        SettingsAdapter.ViewHolder viewHolder = new SettingsAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SettingsAdapter.ViewHolder holder, int position) {
+        final ClassData classDataList = settingsData[position];
+        holder.studentName.setText(classDataList.getClassName());
+        holder.attendance.setText(classDataList.getClassSchedule());
+    }
+
+    @Override
+    public int getItemCount() {
+        return settingsData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView settingName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            settingName = itemView.findViewById(R.id.settingName);
+        }
+    }
+}
--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/settings/SettingsFragment.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.mobdeve.s17.samirsattendanceapp.ClassData;
 import com.mobdeve.s17.samirsattendanceapp.databinding.FragmentSettingsBinding;
+import com.mobdeve.s17.samirsattendanceapp.ui.leaderboard.LeaderboardAdapter;
 
 public class SettingsFragment extends Fragment {
 
@@ -23,6 +26,20 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Initialize the RecyclerView and your custom adapter
+        recyclerView = binding.rvLeaderboardList; // Make sure you have 'recyclerView' in your fragment_home.xml
+        ClassData[] settingsData = new ClassData[]{ // temporary use class data since its placeholders
+                new ClassData("Create Class", ""),
+                new ClassData("Join Class", ""),
+                new ClassData("Edit Class", ""),
+                new ClassData("Settings", "")
+        };
+        SettingsAdapter adapter = new SettingsAdapter(settingsData); // Initialize your adapter, here an empty list is passed
+
+        // Set LayoutManager and Adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
 
 //        final TextView textView = binding.textSettings;
 //        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
