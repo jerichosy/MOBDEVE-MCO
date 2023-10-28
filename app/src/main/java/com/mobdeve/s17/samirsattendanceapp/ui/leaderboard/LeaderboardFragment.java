9adb327dd2810d819cb28656d0124c984b3f711f
Revkxd <79917173+Revkxd@users.noreply.github.com>
2023-10-28 18:03:09 +0800

    recyclerview for leaderboard

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/leaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/leaderboard/LeaderboardFragment.java
@@ -9,12 +9,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.mobdeve.s17.samirsattendanceapp.ClassData;
 import com.mobdeve.s17.samirsattendanceapp.databinding.FragmentLeaderboardBinding;
+import com.mobdeve.s17.samirsattendanceapp.ui.home.ClassAdapter;
 
 public class LeaderboardFragment extends Fragment {
 
     private FragmentLeaderboardBinding binding;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +28,18 @@ public class LeaderboardFragment extends Fragment {
 
         binding = FragmentLeaderboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Initialize the RecyclerView and your custom adapter
+        recyclerView = binding.rvLeaderboardList; // Make sure you have 'recyclerView' in your fragment_home.xml
+        ClassData[] classData = new ClassData[]{ // temporary use class data since its placeholders
+                new ClassData("Samir Car Driver", "50"),
+                new ClassData("Vladimir Cyka Rush", "48")
+        };
+        ClassAdapter adapter = new ClassAdapter(classData); // Initialize your adapter, here an empty list is passed
+
+        // Set LayoutManager and Adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
 
         final TextView textView = binding.textLeaderboard;
         leaderboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
