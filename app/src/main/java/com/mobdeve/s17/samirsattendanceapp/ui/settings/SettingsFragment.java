a1029f451bccfecface5eca65f085a5e43d1dc6e
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-10-28 17:31:00 +0800

    Forgotten as part of renaming from "Notifications" fragment to "Settings"

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/settings/SettingsFragment.java
@@ -24,7 +24,7 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        final TextView textView = binding.textSettings;
         settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
