900df7da4f1acd1317daee2a500d9564b212a46e
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-10-28 17:28:29 +0800

    Rename "Dashboard" fragment to "Leaderboard"

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/leaderboard/LeaderboardViewModel.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/leaderboard/LeaderboardViewModel.java
@@ -1,16 +1,16 @@
-package com.mobdeve.s17.samirsattendanceapp.ui.dashboard;
+package com.mobdeve.s17.samirsattendanceapp.ui.leaderboard;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class LeaderboardViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public LeaderboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is leaderboard fragment");
     }
 
     public LiveData<String> getText() {
