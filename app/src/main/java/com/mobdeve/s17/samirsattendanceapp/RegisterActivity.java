cdc110b8e4ffbd7ccb1bb18fc19713661e4a47a2
zen-young <renzcruz50@gmail.com>
2023-11-25 17:22:58 +0800

    implemented FirebaseAuth registration

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/RegisterActivity.java
@@ -1,13 +1,23 @@
 package com.mobdeve.s17.samirsattendanceapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +26,7 @@ public class RegisterActivity extends AppCompatActivity {
     EditText editTextEmail;
     EditText editTextPassword;
     EditText editTextConfirmPassword;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,53 @@ public class RegisterActivity extends AppCompatActivity {
         editTextEmail = (EditText) findViewById(R.id.register_et_email);
         editTextPassword = (EditText) findViewById(R.id.register_et_password);
         editTextConfirmPassword = (EditText) findViewById(R.id.register_et_confirmPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(this, "User logged in. Redirecting to Login.", Toast.LENGTH_SHORT).show();
+            updateUI(currentUser);
+        }
     }
 
     public void onRegister(View v) {
-        Intent i = new Intent(this, LoginActivity.class);
+        if (!isMatchingPassword()) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
 
-        // Code here to check input, insert DB
-        Toast.makeText(this, "Registration successful! Please login.", Toast.LENGTH_SHORT).show();
+    private boolean isMatchingPassword() {
+        return editTextPassword.getText().toString().equals(editTextConfirmPassword.getText().toString());
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user == null) return;
+        Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
         finish();
     }
