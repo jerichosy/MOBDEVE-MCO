4fe0ee9306408481ecb4724b36b6fc75e851294b
Ced <79917173+Revkxd@users.noreply.github.com>
2023-12-04 18:46:41 +0800

    tweaked attribute names and constructors

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ClassData.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ClassData.java
@@ -1,33 +1,62 @@
 package com.mobdeve.s17.samirsattendanceapp;
 
 public class ClassData {
-    private String className;  // this includes the section
-    private String classSchedule;
+    private String name;  // this includes the section
+    private String schedule;
     private int capacity;
     private int members;
-    private String joinCode;
+    private String join_code;
+    private String creator;
+
+    public ClassData() {
+        // empty constructor needed for firebase
+    }
+
+    public ClassData(String className, String classSchedule) {
+        this.name = className;
+        this.schedule = classSchedule;
+        this.capacity = 0;
+        this.members = 0;
+        this.join_code = "";
+        this.creator = "yes";
+    }
 
     public ClassData(String className, String classSchedule, int capacity, int members, String joinCode) {
-        this.className = className;
-        this.classSchedule = classSchedule;
+        this.name = className;
+        this.schedule = classSchedule;
         this.capacity = capacity;
         this.members = members;
-        this.joinCode = joinCode;
+        this.join_code = joinCode;
+        this.creator = "yes";
+    }
+
+    public ClassData(String className, String classSchedule, int capacity, int members, String joinCode, String creator) {
+        this.name = className;
+        this.schedule = classSchedule;
+        this.capacity = capacity;
+        this.members = members;
+        this.join_code = joinCode;
+        this.creator = creator;
     }
 
     public String getClassName() {
-        return className;
+        return name;
     }
 
     public void setClassName(String className) {
-        this.className = className;
+        this.name = className;
     }
 
     public String getClassSchedule() {
-        return classSchedule;
+        return schedule;
     }
 
     public void setClassSchedule(String classSchedule) {
-        this.classSchedule = classSchedule;
+        this.schedule = classSchedule;
+    }
+
+    @Override
+    public String toString() {
+        return "Class Name: " + name;
     }
 }
