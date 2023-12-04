7cdd09653a4de185d8147749e98cb1b2d4da6b10
Ced <79917173+Revkxd@users.noreply.github.com>
2023-12-04 13:52:07 +0800

    Create Class backend, need setup for getting username

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ClassCreateActivity.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ClassCreateActivity.java
@@ -7,12 +7,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClassCreateActivity extends AppCompatActivity {
 
     EditText et_className;
     EditText et_maxMembers;
     EditText et_classSchedule;
     Button btn_createClass;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +31,29 @@ public class ClassCreateActivity extends AppCompatActivity {
         et_maxMembers = (EditText) findViewById(R.id.et_maxMembers);
         et_classSchedule = (EditText) findViewById(R.id.et_scheduleTime);
         btn_createClass = (Button) findViewById(R.id.btn_createClass);
+        this.db = FirebaseFirestore.getInstance();
         btn_createClass.setOnClickListener(v -> {
-            Toast.makeText(getApplicationContext(), "Class created!", Toast.LENGTH_SHORT).show();
+            String className = et_className.getText().toString();
+            int maxMembers = Integer.parseInt(et_maxMembers.getText().toString());
+            int currentMembers = 0;
+            String classSchedule = et_classSchedule.getText().toString();
+            String classCode = (className.toLowerCase() + " " + classSchedule).replace(" ", "_");
+            Map<String, Object> new_class = new HashMap<>();
+            new_class.put("name", className);
+            new_class.put("capacity", maxMembers);
+            new_class.put("members", currentMembers);
+            new_class.put("schedule", classSchedule);
+            new_class.put("join_code", classCode);
+
+            db.collection("classes").add(new_class).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(getApplicationContext(), "Class created!", Toast.LENGTH_SHORT).show();
+                }
+            })
+            .addOnFailureListener(e -> {
+                Toast.makeText(getApplicationContext(), "Class creation failed!", Toast.LENGTH_SHORT).show();
+            });
             finish();
         });
     }
