8f0fc7105a08a187aed9b5ca97a68a0e91d42680
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-12-04 18:39:16 +0800

    Populate HomeFragment's RecyclerView querying Firestore

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/ClassAdapter.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/ClassAdapter.java
@@ -13,12 +13,14 @@ import com.mobdeve.s17.samirsattendanceapp.ClassData;
 import com.mobdeve.s17.samirsattendanceapp.ClassDetailsActivity;
 import com.mobdeve.s17.samirsattendanceapp.R;
 
+import java.util.List;
+
 public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHolder> {
 
-    ClassData[] classData;
+    private List<ClassData> classDataList;
 
-    public ClassAdapter(ClassData[] classData) {
-        this.classData = classData;
+    public ClassAdapter(List<ClassData> classDataList) {
+        this.classDataList = classDataList;
     }
 
     @NonNull
@@ -33,16 +35,16 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
 
     @Override
     public void onBindViewHolder(@NonNull ClassAdapter.ClassViewHolder holder, int position) {
-        final ClassData classDataList = classData[position];
-        holder.className.setText(classDataList.getClassName());
-        holder.classSchedule.setText(classDataList.getClassSchedule());
+        final ClassData classData = classDataList.get(position);
+        holder.className.setText(classData.getClassName());
+        holder.classSchedule.setText(classData.getClassSchedule());
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(), ClassDetailsActivity.class);
-                i.putExtra("className", classDataList.getClassName());
-                i.putExtra("classSchedule", classDataList.getClassSchedule());
+                i.putExtra("className", classData.getClassName());
+                i.putExtra("classSchedule", classData.getClassSchedule());
                 v.getContext().startActivity(i);
             }
         });
@@ -50,7 +52,7 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
 
     @Override
     public int getItemCount() {
-        return classData.length;
+        return classDataList.size();
     }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
@@ -63,5 +65,11 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
             className = itemView.findViewById(R.id.className);
             classSchedule = itemView.findViewById(R.id.classSchedule);
         }
+    }
+
+    // Method to update the internal list of the adapter
+    public void setClassDataList(List<ClassData> newData) {
+        this.classDataList = newData;
+        notifyDataSetChanged(); // Notify the adapter to refresh the RecyclerView
     }
 }
--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.mobdeve.s17.samirsattendanceapp.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,24 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mobdeve.s17.samirsattendanceapp.ClassData;
 import com.mobdeve.s17.samirsattendanceapp.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private RecyclerView recyclerView;
+    private FirebaseFirestore db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,15 +42,59 @@ public class HomeFragment extends Fragment {
 
         // Initialize the RecyclerView and your custom adapter
         recyclerView = binding.rvClassList; // Make sure you have 'recyclerView' in your fragment_home.xml
-        ClassData[] classData = new ClassData[]{
-            new ClassData("STINTSY S14", "MH 9:15-10:45"),
-            new ClassData("CSOPESY S13", "S 9:15-12:30")
-        };
-        ClassAdapter adapter = new ClassAdapter(classData); // Initialize your adapter, here an empty list is passed
+
+//        ClassData[] classData = new ClassData[]{
+//            new ClassData("STINTSY S14", "MH 9:15-10:45"),
+//            new ClassData("CSOPESY S13", "S 9:15-12:30")
+//        };
+//        ClassAdapter adapter = new ClassAdapter(classData); // Initialize your adapter, here an empty list is passed
+
+        ClassAdapter adapter = new ClassAdapter(Arrays.asList(new ClassData[0]));
 
         // Set LayoutManager and Adapter
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+
+        this.db = FirebaseFirestore.getInstance();
+        db.collection("classes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Create an empty list to hold ClassData objects
+                            List<ClassData> classDataList = new ArrayList<>();
+
+                            // Iterate through the query results
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Extract class name and schedule from the document
+                                String className = Objects.requireNonNull(document.getData().get("name")).toString();
+                                String classSchedule = Objects.requireNonNull(document.getData().get("schedule")).toString();
+//                                System.out.println("\nwowwwww\n");
+//                                System.out.println(className);
+//                                System.out.println(classSchedule);
+                                System.out.println(document.getData());
+
+                                // Create a new ClassData object with the retrieved data
+                                ClassData classData = new ClassData(className, classSchedule);
+
+                                // Add the created ClassData object to the list
+                                classDataList.add(classData);
+                            }
+
+                            // At this point, 'classDataList' contains all ClassData objects
+                            // You can now use this list for your application's purposes
+
+                            // Update the data in adapter and notify the adapter for changes.
+                            adapter.setClassDataList(classDataList);
+                            adapter.notifyDataSetChanged();
+
+                        } else {
+                            // Handle the error
+                            Log.d("FirestoreError", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
