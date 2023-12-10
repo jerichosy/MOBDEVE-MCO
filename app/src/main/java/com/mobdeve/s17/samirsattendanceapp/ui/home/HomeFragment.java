c8ee5299f801b74f779c09a98ad0506b9b03760b
zen-young <renzcruz50@gmail.com>
2023-12-11 07:20:55 +0800

    added new RecyclerView for upcoming classes

--- app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/mobdeve/s17/samirsattendanceapp/ui/home/HomeFragment.java
@@ -20,17 +20,24 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.mobdeve.s17.samirsattendanceapp.ClassData;
+import com.mobdeve.s17.samirsattendanceapp.TimeParser;
 import com.mobdeve.s17.samirsattendanceapp.databinding.FragmentHomeBinding;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private RecyclerView recyclerView;
+    private RecyclerView rvClassList;
+    private RecyclerView rvUpcomingList;
     private FirebaseFirestore db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -42,13 +49,17 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         // Initialize the RecyclerView and your custom adapter
-        recyclerView = binding.rvClassList; // Make sure you have 'recyclerView' in your fragment_home.xml
+        rvClassList = binding.rvClassList; // Make sure you have 'recyclerView' in your fragment_home.xml
+        rvUpcomingList = binding.rvUpcomingList;
 
-        ClassAdapter adapter = new ClassAdapter(Arrays.asList(new ClassData[0]));
+        ClassAdapter classListAdapter = new ClassAdapter(Arrays.asList(new ClassData[0]));
+        ClassAdapter upcomingListAdapter = new ClassAdapter(Arrays.asList(new ClassData[0]));
 
         // Set LayoutManager and Adapter
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
+        rvClassList.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvClassList.setAdapter(classListAdapter);
+        rvUpcomingList.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvUpcomingList.setAdapter(upcomingListAdapter);
 
         this.db = FirebaseFirestore.getInstance();
         String uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
@@ -59,22 +70,29 @@ public class HomeFragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             // Create an empty list to hold ClassData objects
-                            List<ClassData> classDataList = new ArrayList<>();
+                            List<ClassData> classList = new ArrayList<>();
+                            List<ClassData> upcomingList = new ArrayList<>();
 
                             // Iterate through the query results
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 ClassData classData = document.toObject(ClassData.class);
                                 // Add the created ClassData object to the list if the user is a member
                                 if (classData.getClassMembers().contains(uid) || classData.getClassCreator().contains(uid))
-                                    classDataList.add(classData);
+                                    classList.add(classData);
                             }
 
                             // At this point, 'classDataList' contains all ClassData objects
                             // You can now use this list for your application's purposes
+                            for (ClassData item : classList) {
+                                // TODO: Filter based on time
+                                upcomingList.add(item);
+                            }
 
                             // Update the data in adapter and notify the adapter for changes.
-                            adapter.setClassDataList(classDataList);
-                            adapter.notifyDataSetChanged();
+                            classListAdapter.setClassDataList(classList);
+                            classListAdapter.notifyDataSetChanged();
+                            upcomingListAdapter.setClassDataList(upcomingList);
+                            upcomingListAdapter.notifyDataSetChanged();
 
                         } else {
                             // Handle the error
